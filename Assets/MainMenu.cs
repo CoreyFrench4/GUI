0edb95f80ecfc249ca17efb3dcb1b193262c9fe4
CoreyFrench4 <30771822+CoreyFrench4@users.noreply.github.com>
2017-08-15 13:03:04 +1000

    Added project to Repos

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    #region Variables
+    public bool gameScene, showOptions;
+    public GameObject menu, options;
+    public Slider volumeSlider, brightnessSlider;
+    public AudioSource music;
+    public Light dirLight;
+    #endregion
+     void Start()
+    {
+        if(PlayerPrefs.HasKey("Volume"))
+        {
+            Load();
+        }
+        if (volumeSlider != null && music != null)
+        {
+                       
+                volumeSlider.GetComponent<Slider>().value = music.volume;
+            
+        }
+        
+        if (brightnessSlider != null && dirLight != null)
+        {
+            brightnessSlider.value = dirLight.intensity;
+
+        }
+      
+    }
+
+    void Update()
+    {
+        if (volumeSlider != null && music != null)
+        {
+            if (music.volume != volumeSlider.value)
+                music.volume = volumeSlider.value;
+        }
+        if (brightnessSlider != null && dirLight != null)
+        {
+            if(brightnessSlider.value != dirLight.intensity)
+            {
+                dirLight.intensity = brightnessSlider.value;
+            }
+        }
+    }
+
+    
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+   
+    public void Microtransactions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ShowOptions()
+    {
+        ToggleOptions();
+    }
+    public bool ToggleOptions()
+    {
+        if (showOptions)
+        {
+            showOptions = false;
+            menu.SetActive(true);
+            options.SetActive(false);
+            return false;
+        }
+        else
+        {
+            showOptions = true;
+            menu.SetActive(false);
+            options.SetActive(true);
+            return true;
+        }
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("Volume", music.volume);
+        PlayerPrefs.SetFloat("Brightness", dirLight.intensity);
+    }
+    public void Load()
+    {
+        music.volume = PlayerPrefs.GetFloat("Volume");
+        dirLight.intensity = PlayerPrefs.GetFloat("Brightness");
+    }
+    public void Default()
+    {
+        volumeSlider.value = 1;
+        brightnessSlider.value = 1;
+        PlayerPrefs.SetFloat("Volume", 1);
+        PlayerPrefs.SetFloat("Brightness", 1);
+    }
+}
+
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public Slider volumeSlider, brightnessSlider;
+    public AudioSource music;
+    public Light dirLight;
+    public bool gameScene, showOptions;
+    public GameObject pause, options;
+   
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+    public void ShowOptions()
+    {
+        ToggleOptions();
+    }
+
+    public bool ToggleOptions()
+    {
+        if (showOptions)
+        {
+            showOptions = false;
+            pause.SetActive(true);
+            options.SetActive(false);
+            return false;
+        }
+        else
+        {
+            showOptions = true;
+           pause.SetActive(false);
+            options.SetActive(true);
+            return true;
+        }
+    
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("Volume", music.volume);
+        PlayerPrefs.SetFloat("Brightness", dirLight.intensity);
+    }
+    public void Load()
+    {
+        music.volume = PlayerPrefs.GetFloat("Volume");
+        dirLight.intensity = PlayerPrefs.GetFloat("Brightness");
+    }
+    public void Default()
+    {
+        volumeSlider.value = 1;
+        brightnessSlider.value = 1;
+        PlayerPrefs.SetFloat("Volume", 1);
+        PlayerPrefs.SetFloat("Brightness", 1);
+    }
+
+}
+
+    
+    
+    
+
+
